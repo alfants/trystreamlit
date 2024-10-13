d1ce571584b9aa59eab3534d3c48c53f159d3a97
M. Alfan Tsalits <153008876+alfants@users.noreply.github.com>
2024-10-13 17:29:50 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="BRS Report", page_icon=":four_leaf_clover", layout="wide")
+
+# --- HEADER ---
+with st.container():
+    st.subheader("Hi, I am Alfan :wave:")
